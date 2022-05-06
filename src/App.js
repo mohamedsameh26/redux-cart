a276be1a98f6144272fc0d2c294f65e99c80a01d
mohamedsameh26 <https://github.com/mohamedsameh26>
2022-05-06 19:47:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,33 @@ import React from "react";
 // components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
-// items
-import cartItems from "./cart-items";
+
 // redux stuff
+// Store - stores data, think of state
+// reducer - function that used to update store
+// state - old state/state before update
+// action - whate happened / what update
+// return updated or old state
+
+import {createStore} from 'redux';
+import reducer from "./reducer";
+import {Provider} from 'react-redux';
+// initial store
+
+
+
+
+// store
+const store = createStore(reducer);
 
 function App() {
-  // cart setup
+  // cart setup 
 
   return (
-    <main>
+    <Provider store={store}>
       <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
+      <CartContainer />
+    </Provider>
   );
 }
 
